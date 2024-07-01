faa144dd3404c4812475f236c6695601a7a9d698
OluC94 <coker.olu94@gmail.com>
2024-07-01 13:49:44 +0100

    add styling to prev generations

--- src/components/RecentCodes.jsx
+++ src/components/RecentCodes.jsx
@@ -1,16 +1,17 @@
 export default function RecentCodes(props) {
-    console.log(props.lastFiveCodes)
-
-    const codeList = props.lastFiveCodes.map((code, index) => {
-        return (<div key={index}>
-            <p>{code.text}</p>
-            <img src={code.url} alt={`QR Code for "${code.text}"`} />
-        </div>)
-    })
+  const codeList = props.lastFiveCodes.map((code, index) => {
     return (
-        <div>
-            <h3>Previous Generations</h3>
-            {codeList}
-        </div>
-    )
+      <div className="code-tile" key={index}>
+        <p>{code.text}</p>
+        <img src={code.url} alt={`QR Code for "${code.text}"`} />
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h3>Previous Generations</h3>
+      {codeList}
+    </div>
+  );
 }
