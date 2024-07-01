b4efb000338d44ff5ec7cd4e35043f705989de9a
OluC94 <coker.olu94@gmail.com>
2024-07-01 13:31:16 +0100

    remove components

--- src/QRCodeGenerator.jsx
+++ src/QRCodeGenerator.jsx
@@ -1,18 +1,34 @@
 import { useState } from "react"
-import InputArea from "./components/InputArea"
-import QRCodeArea from "./components/QRCodeArea"
+import QRCode from "qrcode"
 
 export default function QRCodeGenerator(){
+    const [inputUrl, setInputUrl] = useState("")
     const [myDataURL, setMyDataURL] = useState("")
 
-    return (
-        <div>
-          <h1>QR Code Generator</h1>
-          <InputArea 
-            setMyDataURL={setMyDataURL}
-            myDataURL={myDataURL}
-          />
-          <QRCodeArea myDataURL={myDataURL}/>
-        </div>
-      )
+    function handleUrlInput(event){
+        setInputUrl(event.target.value)
+    }
+
+    function handleGenerateClicked() {
+        generateQRCode(inputUrl)
+        
+    }
+
+    function generateQRCode(link) {
+        return QRCode.toDataURL(link).then(generatedURL => {
+            setMyDataURL(generatedURL)
+            return generatedURL
+        })
+    }
+
+    return (<div>
+        <h1>QR Code Generator</h1>
+            <input
+                type="text"
+                placeholder="https://www..."
+                onChange={handleUrlInput}
+            />
+            <button onClick={handleGenerateClicked}>Generate</button>
+            {myDataURL && <img src={myDataURL} alt={`QR Code for ${inputUrl}`}/>}
+    </div>)
 }
